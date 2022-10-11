62dccea509951637370568af5ddb1c97a610746a
Brian Ore <orebrian95@gmail.com>
2022-10-10 23:37:04 -0300

    se agrego el js con el array, y se implemen el map

--- src/bookAPI/bookAPI.js
+++ src/bookAPI/bookAPI.js
@@ -1,290 +1,271 @@
 const data = [
   {
-    id
-      : '1',
+    id: 1,
     titulo: 'libro 1',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 1200,
     autor: 'autor 1',
-    img: './img/stock/Attack_on_Titan_Lost_Girls_(novel).jpg',
+    img: '../../Images/Books/Attack_on_Titan_Lost_Girls_(novel).jpg',
     categoria: 'a'
   },
   {
-    id
-      : '2',
+    id: 2,
     titulo: 'libro 2',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 1100,
     autor: 'autor 2',
-    img: './img/stock/Back_When_You_Called_Us_Devils_Volume_13.jpg',
+    img: '../../Images/Books/Back_When_You_Called_Us_Devils_Volume_13.jpg',
     categoria: 'a'
   },
   {
-    id
-      : '3',
+    id: 3,
     titulo: 'libro 3',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 1200,
     autor: 'autor 3',
-    img: './img/stock/Cardcaptor_Sakura_Clear_Card_Volume_11.jpg',
+    img: '../../Images/Books/Cardcaptor_Sakura_Clear_Card_Volume_11.jpg',
     categoria: 'a'
   },
   {
-    id
-      : '4',
+    id: 4,
     titulo: 'libro 4',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 1400,
     autor: 'autor 4',
-    img: './img/stock/DAYS_Volume_29.jpg',
+    img: '../../Images/Books/DAYS_Volume_29.jpg',
     categoria: 'b'
   },
   {
-    id
-      : '5',
+    id: 5,
     titulo: 'libro 5',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 1200,
     autor: 'autor 1',
-    img: './img/stock/Hagakure.jpg',
+    img: '../../Images/Books/Hagakure.jpg',
     categoria: 'b'
   },
   {
-    id
-      : '6',
+    id: 6,
     titulo: 'libro 6',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 1500,
     autor: 'autor 2',
-    img: './img/stock/Harem_Marriage_Volume_17.jpg',
+    img: '../../Images/Books/Harem_Marriage_Volume_17.jpg',
     categoria: 'c'
   },
   {
-    id
-      : '7',
+    id: 7,
     titulo: 'libro 7',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 500,
     autor: 'autor 3',
-    img: './img/stock/Koigakubo-kun_Stole_My_First_Time_Volume_1.jpg',
+    img: '../../Images/Books/Koigakubo-kun_Stole_My_First_Time_Volume_1.jpg',
     categoria: 'c'
   },
   {
-    id
-      : '8',
+    id: 8,
     titulo: 'libro 8',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 500,
     autor: 'autor 4',
-    img: './img/stock/Musashi.jpg',
+    img: '../../Images/Books/Musashi.jpg',
     categoria: 'd'
   },
   {
-    id
-      : '9',
+    id: 9,
     titulo: 'libro 9',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 500,
     autor: 'autor 1',
-    img: './img/stock/Nighttime_for_Just_Us_Two_Volume_2.jpg',
+    img: '../../Images/Books/Nighttime_for_Just_Us_Two_Volume_2.jpg',
     categoria: 'd'
   },
   {
-    id
-      : '10',
+    id: 10,
     titulo: 'libro 10',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 700,
     autor: 'autor 2',
-    img: './img/stock/OTOMO_A_Global_Tribute_to_the_Mind_Behind_Akira.jpg',
+    img: '../../Images/Books/OTOMO_A_Global_Tribute_to_the_Mind_Behind_Akira.jpg',
     categoria: 'e'
   },
   {
-    id
-      : '11',
+    id: 11,
     titulo: 'libro 11',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 700,
     autor: 'autor 3',
-    img: './img/stock/Seraph_of_the_End_Guren_Ichinose_Resurrection_at_Nineteen_Volume_1.jpg',
+    img: '../../Images/Books/Seraph_of_the_End_Guren_Ichinose_Resurrection_at_Nineteen_Volume_1.jpg',
     categoria: 'e'
   },
   {
-    id
-      : '12',
+    id: 12,
     titulo: 'libro 12',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 700,
     autor: 'autor 4',
-    img: './img/stock/Taiko.jpg',
+    img: '../../Images/Books/Taiko.jpg',
     categoria: 'f'
   },
   {
-    id
-      : '13',
+    id: 13,
     titulo: 'libro 13',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 900,
     autor: 'autor 1',
-    img: './img/stock/The_Blade_of_the_Courtesans.jpg',
+    img: '../../Images/Books/The_Blade_of_the_Courtesans.jpg',
     categoria: 'f'
   },
   {
-    id
-      : '14',
+    id: 14,
     titulo: 'libro 14',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 1400,
     autor: 'autor 2',
-    img: './img/stock/The_Seven_Deadly_Sins_Seven-Colored_Recollections.jpg',
+    img: '../../Images/Books/The_Seven_Deadly_Sins_Seven-Colored_Recollections.jpg',
     categoria: 'g'
   },
   {
-    id
-      : '15',
+    id: 15,
     titulo: 'libro 15',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 7000,
     autor: 'autor 3',
-    img: './img/stock/Your_Lie_in_April_A_Six-Person_Etude.jpg',
+    img: '../../Images/Books/Your_Lie_in_April_A_Six-Person_Etude.jpg',
     categoria: 'g'
   },
   {
-    id
-      : '16',
+    id: 16,
     titulo: 'libro 16',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 777,
     autor: 'autor 4',
-    img: './img/stock/Ring.jpg',
+    img: '../../Images/Books/Ring.jpg',
     categoria: 'h'
   },
   {
-    id
-      : '17',
+    id: 17,
     titulo: 'libro 17',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 234,
     autor: 'autor 4',
-    img: './img/stock/Shield_of_Straw.jpg',
+    img: '../../Images/Books/Shield_of_Straw.jpg',
     categoria: 'h'
   },
   {
-    id
-      : '18',
+    id: 18,
     titulo: 'libro 18',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 155600,
     autor: 'autor 1',
-    img: './img/stock/The_Eternal_Zero.jpg',
+    img: '../../Images/Books/The_Eternal_Zero.jpg',
     categoria: 'i'
   },
   {
-    id
-      : '19',
+    id: 19,
     titulo: 'libro 19',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 1600,
     autor: 'autor 2',
-    img: './img/stock/Lala_Pipo.jpg',
+    img: '../../Images/Books/Lala_Pipo.jpg',
     categoria: 'i'
   },
   {
-    id
-      : '20',
+    id: 20,
     titulo: 'libro 20',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 3200,
     autor: 'autor 3',
-    img: './img/stock/Lala_Pipo.jpg',
+    img: '../../Images/Books/Lala_Pipo.jpg',
     categoria: 'j'
   },
   {
-    id
-      : '21',
+    id: 21,
     titulo: 'libro 21',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 2300,
     autor: 'autor 4',
-    img: './img/stock/DAYS_Volume_29.jpg',
+    img: '../../Images/Books/DAYS_Volume_29.jpg',
     categoria: 'j'
   },
   {
-    id
-      : '22',
+    id: 22,
     titulo: 'libro 22',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 5600,
     autor: 'autor 1',
-    img: './img/stock/Cardcaptor_Sakura_Clear_Card_Volume_11.jpg',
+    img: '../Images/Books/Cardcaptor_Sakura_Clear_Card_Volume_11.jpg',
     categoria: 'k'
   },
   {
-    id
-      : '23',
+    id: 23,
     titulo: 'libro 23',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 1700,
     autor: 'autor 2',
-    img: './img/stock/Back_When_You_Called_Us_Devils_Volume_13.jpg',
+    img: '../../Images/Books/Back_When_You_Called_Us_Devils_Volume_13.jpg',
     categoria: 'k'
   },
   {
-    id
-      : '24',
+    id: 24,
     titulo: 'libro 24',
     tipo: 'libro',
     stock: 1,
     desc: 'Desc',
     precio: 800,
     autor: 'autor 3',
-    img: './img/stock/Hagakure.jpg',
+    img: '../../Images/Books/Hagakure.jpg',
     categoria: 'l'
   }
-]
+];
+export function getBooks() {
+  return new Promise((resolver) => {
+    setTimeout(() => resolver(data), 1500)
+  })
+}
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,34 +1,35 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ItemCard from '../ItemCard/ItemCard';
 import './ItemListContainer.css'
-
+import { getBooks } from '../../bookAPI/bookAPI';
 
 
 export default function ItemListContainer() {
 
+  const [dataBook, setDataBook] = useState([]);
+
+  useEffect(() => {
+    getBooks().then((data) => {
+      setDataBook(data);
+    });
+  }, [])
 
   return (
     <div className='list-item'>
-      <ItemCard
-        img="https://briano-lab.github.io/Pre_Entrega2_JS/img/stock/Attack_on_Titan_Lost_Girls_(novel).jpg"
-        titulo="Libro 1"
-        autor="Autor 1"
-        precio="1200"
-      />
-      <ItemCard
-        img="https://briano-lab.github.io/Pre_Entrega2_JS/img/stock/Back_When_You_Called_Us_Devils_Volume_13.jpg"
-        titulo="Libro 2"
-        autor="Autor 2"
-        precio="1200"
-      />
-      <ItemCard
-        img="https://briano-lab.github.io/Pre_Entrega2_JS/img/stock/Cardcaptor_Sakura_Clear_Card_Volume_11.jpg"
-        titulo="Libro 3"
-        autor="Autor 3"
-        precio="1200"
-      />
-   
+      {dataBook.map((book) => {
+        return (
+          <ItemCard
+            key={book.id}
+            titulo={book.titulo}
+            img={book.img}
+            autor={book.autor}
+            precio={book.precio}
+          />
+        );
+      })}
+
+
 
     </div>
   )
